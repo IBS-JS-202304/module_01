19639bd8d7cda06eb0182a31f58b95435aff752c
Лепнев Павел Алексеевич <PLepnev@IBS.RU>
2023-04-20 13:45:42 +0300

    mock-data to data hooks

--- src/components/EmployeeList/EmployeeList.jsx
+++ src/components/EmployeeList/EmployeeList.jsx
@@ -1,9 +1,24 @@
-import { EmployeeList as data } from '../../store/EmployeeList';
+import { useCallback } from 'react';
+import { useEmployeeList } from '../../store/useEmployeeList';
 import { EmployeeListItem } from './EmployeeListItem';
 
 export const EmployeeList = () => {
-    return (<div className="employee-list-wrapper">{data.map((user, i) => <EmployeeListItem
-        user={user}
-        key={`${i}-${user.name}`}
-    />)}</div>);
+    const { data, isFetching, isLoaded } = useEmployeeList();
+
+    const prepareList = useCallback(() => {
+        if (isFetching) {
+            return <>Data loading...</>;
+        } else if (!isFetching && isLoaded && data) {
+            return <>{data.map((user, i) => <EmployeeListItem
+                user={user}
+                key={`${i}-${user.name}`}
+            />)}</>
+        }
+
+        return <>No data.</>;
+    }, [data, isFetching, isLoaded]);
+
+    return (<div className="employee-list-wrapper">
+        {prepareList()}
+    </div>);
 }
