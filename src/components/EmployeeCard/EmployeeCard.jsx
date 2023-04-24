1bb005c5e72b7370025d0a3f0dbebf40897d01d0
Лепнев Павел Алексеевич <PLepnev@IBS.RU>
2023-04-24 13:54:56 +0300

    allow edit employee

--- src/components/EmployeeCard/EmployeeCard.jsx
+++ src/components/EmployeeCard/EmployeeCard.jsx
@@ -1,19 +1,55 @@
+import { useEffect, useCallback } from 'react';
 import { useParams } from "react-router-dom";
 import { useEmployeeFullData } from '../../store/useEmployeeFullData';
 import { EmployeeCardElement } from './EmployeeCardElement';
 import './EmployeeCard.css';
-import { useEffect } from 'react';
+import { useState } from 'react';
 
 export const EmployeeCard = () => {
+    const [employee, setEmployee] = useState();
     const { employeeId } = useParams();
+    const { data, isFetching, isLoaded, getEmployee, } = useEmployeeFullData();
 
-    const { data: employee, isFetching, isLoaded, getEmployee, } = useEmployeeFullData();
+    const prepOnSave = useCallback((fieldName) => (newValue) => {
+        switch (fieldName) {
+            case 'email':
+                setEmployee((state) => { return { ...state, email: newValue } });
+                break;
+            case 'phone-office':
+                setEmployee((state) => {
+                    const newState = { ...state };
+                    newState.phone.office = newValue;
+                    return newState;
+                });
+                break;
+            case 'phone-cell':
+                setEmployee((state) => {
+                    const newState = { ...state };
+                    newState.phone.cell = newValue;
+                    return newState;
+                });
+                break;
+            case 'phone-sms':
+                setEmployee((state) => {
+                    const newState = { ...state };
+                    newState.phone.sms = newValue;
+                    return newState;
+                });
+                break;
+        }
+    }, [setEmployee]);
 
     useEffect(() => {
-        if (!employee && !isFetching && !isLoaded && employeeId) {
+        if (!isFetching && !isLoaded && employeeId) {
             getEmployee(employeeId);
         }
-    }, [employee, isFetching, isLoaded, getEmployee, employeeId]);
+    }, [isFetching, isLoaded, getEmployee, employeeId]);
+
+    useEffect(() => {
+        if (data) {
+            setEmployee(data);
+        }
+    }, [data, setEmployee]);
 
     return (<div className="employee-page-wrapper">
         {employee && (<div className="employee-info-wrapper">
@@ -24,10 +60,10 @@ export const EmployeeCard = () => {
                     <div className='employee-position'>{employee.position}</div>
                 </div>
             </div>
-            <EmployeeCardElement label="Email" value={employee.email} onEditClick={() => { }} />
-            <EmployeeCardElement label="Call office" value={employee.phone.office} onEditClick={() => { }} />
-            <EmployeeCardElement label="Call mobile" value={employee.phone.cell} onEditClick={() => { }} />
-            <EmployeeCardElement label="SMS" value={employee.phone.sms} onEditClick={() => { }} />
+            <EmployeeCardElement label="Email" value={employee.email} onSave={prepOnSave('email')} />
+            <EmployeeCardElement label="Call office" value={employee.phone.office} onSave={prepOnSave('phone-office')} />
+            <EmployeeCardElement label="Call mobile" value={employee.phone.cell} onSave={prepOnSave('phone-cell')} />
+            <EmployeeCardElement label="SMS" value={employee.phone.sms} onSave={prepOnSave('phone-sms')} />
         </div>)}
 
         {(!employee && isFetching) && (<>Employee data loading...</>)}
