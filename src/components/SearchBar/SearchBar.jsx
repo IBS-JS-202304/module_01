b17a64eeb799c8b313c513701f0b0beaa6e0fa33
Makarov Andrey <winandfx@gmail.com>
2023-04-24 17:12:48 +0300

    fix: ref dependency

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -3,17 +3,17 @@ import './SearchBar.css';
 
 export const SearchBar = () => {
     const [isSearchDisabled, setIsSearchDisabled] = useState(false);
-    const inputTextElement = useRef(null);  // https://react.dev/learn/manipulating-the-dom-with-refs
+    const inputTextElementRef = useRef(null);  // https://react.dev/learn/manipulating-the-dom-with-refs
 
     const doSearch = useCallback(() => {
         setIsSearchDisabled(true);
-        console.log("Search request is", inputTextElement.current.value);
+        console.log("Search request is", inputTextElementRef.current.value);
         setTimeout(() => { setIsSearchDisabled(false) }, 2000);
-    }, [setIsSearchDisabled]);
+    }, [setIsSearchDisabled, inputTextElementRef]);
 
     return (<div className="search-wrapper">
         <div className="search-input-wrapper">
-            <input ref={inputTextElement} className="search-input" type="text" disabled={isSearchDisabled} />
+            <input ref={inputTextElementRef} className="search-input" type="text" disabled={isSearchDisabled} />
         </div>
         <div><button disabled={isSearchDisabled} onClick={doSearch}>Find this!</button></div>
     </div>
