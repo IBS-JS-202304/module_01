874a84d757034e6db15a65484b97f7d662c4dbf9
Лепнев Павел Алексеевич <PLepnev@IBS.RU>
2023-04-27 13:59:21 +0300

    code repair

--- src/store/employeeList/employeeListSlice.js
+++ src/store/employeeList/employeeListSlice.js
@@ -1,19 +1,61 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { mock } from './employeeListMock';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { getEmployeeListQuery } from './client';
 
-export const employeeListSlice = createSlice({
+const extraActions = {
+    getList: createAsyncThunk(`getEmployeeList`, getEmployeeListQuery)
+}
+
+const employeeListSlice = createSlice({
     name: 'employeeList',
     initialState: {
         data: [],
+        pending: false,
+        loaded: false,
+        error: false,
     },
     reducers: {
-        getList: (state, action) => {
-            state.data = mock;
+        addEmployee: (state, { payload }) => {
+            const { id } = state.data.at(-1);
+            state.data.push({
+                ...payload,
+                id: id + 1
+            });
+        },
+        updateEmployee: (state, { payload }) => {
+            const targetIndex = state.data.findIndex(({ id }) => id === payload.id);
+            if (targetIndex > -1) {
+                state.data[targetIndex] = payload;
+            }
         }
     },
+    extraReducers: (builder) => {
+        builder
+            .addCase(extraActions.getList.pending, (state) => {
+                state.data = [];
+                state.pending = true;
+                state.loaded = false;
+                state.error = false;
+
+            })
+            .addCase(extraActions.getList.fulfilled, (state, { payload }) => {
+                state.data = payload;
+                state.pending = false;
+                state.loaded = true;
+                state.error = false;
+            })
+            .addCase(extraActions.getList.rejected, (state, { error }) => {
+                console.error(error);
+                state.data = [];
+                state.pending = false;
+                state.loaded = false;
+                state.error = true;
+
+            });
+    }
 })
 
 // Action creators are generated for each case reducer function
-export const { getList } = employeeListSlice.actions;
+export const { getList } = extraActions;
+export const { addEmployee, updateEmployee } = employeeListSlice.actions;
 
 export default employeeListSlice.reducer;
